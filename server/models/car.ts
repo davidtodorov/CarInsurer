534a2fbaa4877e59d943a73230183ddb2c16977d
davidtodorov <t_david@outlook.com>
2021-11-05 09:33:43 +0200

    car plate validator

--- server/models/car.ts
+++ server/models/car.ts
@@ -3,16 +3,24 @@ import { IUser } from './user';
 
 const { String, Number, Date, ObjectId } = Schema.Types;
 
-export interface ICar extends Document{
+export interface ICar extends Document {
     plateNumber: String;
     productionYear: Date;
     owner: IUser['id'];
 }
 
-const carSchema =  new Schema<ICar>({
-    plateNumber: { type: String, required: true },
-    productionYear: { type: Date, required: true},
-    owner: { type: ObjectId, ref: 'User',  required: true },
+const carSchema = new Schema<ICar>({
+    plateNumber: {
+        type: String, required: true,
+        validate: {
+            validator: function (v: any) {
+                return /^(E|A|B|BT|BH|BP|EB|TX|K|KH|OB|M|PA|PK|EH|PB|PP|P|CC|CH|CO|C|CA|CB|CT|T|X|H|Y)(\d{4})([A|B|E|K|M|H|O|P|C|T|Y|X]{2})$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    },
+    productionYear: { type: Date, required: true },
+    owner: { type: ObjectId, ref: 'User', required: true },
 });
 
 export const Car = model<ICar>('Car', carSchema);
--- server/models/user.ts
+++ server/models/user.ts
@@ -2,7 +2,7 @@ import { Schema, Document, model } from 'mongoose';
 
 const { String, Number } = Schema.Types;
 
-export interface IUser extends Document{
+export interface IUser extends Document {
     firstName: String;
     middleName: String;
     lastName: String;
@@ -12,7 +12,10 @@ const UserSchema = new Schema<IUser>({
     firstName: { type: String, required: true },
     middleName: { type: String },
     lastName: { type: String, required: true },
-    identityNumber: { type: Number, required: true }
+    identityNumber: {
+        type: Number, required: true,
+        
+    }
 });
 
 export const User = model<IUser>('User', UserSchema);
