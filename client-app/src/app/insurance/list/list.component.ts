821e01f70f9e7d0c72a82593bddc6caecb04eae6
davidtodorov <t_david@outlook.com>
2021-12-03 10:27:40 +0200

    add sorting to insurances table

--- client-app/src/app/insurance/list/list.component.ts
+++ client-app/src/app/insurance/list/list.component.ts
@@ -1,35 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { InsuranceService } from '../services/insurance.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-insurance-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit {
   constructor(private insuranceService: InsuranceService) { }
 
   public insurances = new MatTableDataSource<any>([]);
   public displayedColumns: string[] = ['owner', 'plateNumber', 'startDate', 'endDate', 'cost', 'dueAmount', 'actions']
 
+  @ViewChild(MatSort) sort!: MatSort;
+
   ngOnInit(): void {
     this.insuranceService.loadInsurances().subscribe(insuranes => {
       this.insurances.data = insuranes;
-      console.log(this.insurances)
     });
 
-    this.insurances.filterPredicate = function(data, filter: string): boolean {
-      return data.car.owner.firstName.toLowerCase().includes(filter) || 
-             data.car.owner.lastName.toLowerCase().includes(filter) || 
-             data.car.plateNumber.toLowerCase().includes(filter) ||
-             data.cost.toString().toLowerCase().includes(filter) ||
-             data.dueAmount.toString().toLowerCase().includes(filter)
-    };
+    this.registerDataSourceFilterHandler();
+    this.registerDataSourceSortDataAccessor();
   }
 
-  doFilter(value: any) {
+  ngAfterViewInit(): void {
+    this.insurances.sort = this.sort;
+  }
+
+  applyFilter(value: any) {
     this.insurances.filter = value.target.value.trim().toLocaleLowerCase();
   }
 
+  private registerDataSourceFilterHandler() {
+    this.insurances.filterPredicate = function (data, filter: string): boolean {
+      return data.car.owner.firstName.toLowerCase().includes(filter) ||
+        data.car.owner.lastName.toLowerCase().includes(filter) ||
+        data.car.plateNumber.toLowerCase().includes(filter) ||
+        data.cost.toString().toLowerCase().includes(filter) ||
+        data.dueAmount.toString().toLowerCase().includes(filter)
+    };
+  }
+
+  private registerDataSourceSortDataAccessor() {
+    this.insurances.sortingDataAccessor = function (data: any, sortHeaderId: string) {
+      if (sortHeaderId === 'owner') {
+        return `${data.car.owner.firstName.toLowerCase()} ${data.car.owner.lastName.toLowerCase()}`
+      }
+      else if (sortHeaderId = 'plateNumber') {
+        return data.car.plateNumber;
+      }
+      return data[sortHeaderId];
+    }
+  }
 }
